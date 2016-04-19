226d44161676cc6b6ca4df88d7b61d353dccb69e
Jonas <jonas.remgard@gmail.com>
2016-04-19 15:05:16 +0200

    addded button links on start page

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,7 +1,15 @@
 class ProposalsController < ApplicationController
 
   def index
-    @proposals = Proposal.all 
+    @proposals = Proposal.all
   end
+
+  def new
+    @newproposal = Proposal.new
+
+    def create
+      @newproposal = Proposal.new(params)
+
+end
 
 end
