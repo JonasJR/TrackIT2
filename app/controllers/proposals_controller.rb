abc9222a4377739b8808082d73c961394153e5ff
Jonas <jonas.remgard@gmail.com>
2016-04-19 15:35:03 +0200

    submitting is working

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -8,4 +8,22 @@ class ProposalsController < ApplicationController
     @newproposal = Proposal.new
 end
 
+def create
+  proposal = Proposal.new(proposal_params)
+
+if proposal.save
+  redirect_to root_url
+  flash[:success] = "Proposal submited with no errors"
+else
+  render :new
+  flash[:error] = "An error occured, please try again!"
+end
+end
+
+private
+
+def proposal_params
+  params.require(:proposal).permit(:name, :description)
+end
+
 end
