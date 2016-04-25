7bde1a11f05f07191ca3627f7c933d85f25e21a5
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2016-04-25 13:48:39 +0200

    Fix resource bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def is_active?(link_path)
    current_page?(link_path) ? "active" : ""
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  
 end
