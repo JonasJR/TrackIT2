e0d5ea9c26aff4c805e31eab0a62c8eea56c958f
Marten <persson_dunk@msn.com>
2016-04-28 13:36:52 +0200

    spanColor on Submit page

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160420121912) do
     t.text     "description"
     t.string   "category"
     t.string   "course"
+    t.string   "company"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
