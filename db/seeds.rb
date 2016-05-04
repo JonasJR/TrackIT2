991cf152d64d95e46bc76a3e6a69fffe9ea71eb0
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2016-05-04 14:30:19 +0200

    Add db:seed functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# id, name, desc, category, course, company, approved, vsc_url, project_managment_service
 proposals = [
-  { user_id: 1, name: 'Test', description: 'test', category: 'Thesis', course: 'DA339A', company: '', approved: false, vsc_url: '', project_managment_service: '' }
+  [1, 'Track IT', 'flksdajflsdkafjsad dsfkj adslkfjdsalkf dsf klds jfkldsfj klds jfkldsjf ldsa fjlkds f', 'Mah', '', '', false, '', '' ],
+  [1, 'Skånetrafiken', 'App för att fixa nätverkstrafiken på skånetrafiken. ', '', '', '', false, '', '' ]
 ]
 
-proposals.each do |name, population|
-  Country.create( name: name, population: population )
+proposals.each do |id, name, desc, category, course, company, approved, vsc_url, project_managment_service|
+  Proposal.create( user_id: id, name: name, description: desc, category: category, course: course, company: company, approved: approved, vsc_url: vsc_url, project_managment_service: project_managment_service )
 end
