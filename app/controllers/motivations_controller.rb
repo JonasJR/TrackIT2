0572e32b7e41999816ee148fa99f5b23e265327c
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2016-05-17 14:41:45 +0200

    Add remove functionality to motivations

--- app/controllers/motivations_controller.rb
+++ app/controllers/motivations_controller.rb
@@ -1,13 +1,30 @@
 class MotivationsController < ActionController::Base
 
   def create
-    
+
+  end
+
+  def show
+  end
+
+  def destroy
+    session[:return_to] ||= request.referer
+    if current_user.try(:teacher)
+      motivation = Motivation.find(params[:id])
+      motivation.destroy
+      redirect_to session.delete(:return_to)
+    else
+      redirect_to session.delete(:return_to), notice: 'You must be logged in as i teacher to delete motivations.'
+    end
   end
 
   private
 
-  def motivation_params
-    params.require(:motivation).permit(:motivation)
-  end
+    def motivation_params
+      params.require(:motivation).permit(:id, :motivation)
+    end
+
+    def check_teacher!
+    end
 
 end
