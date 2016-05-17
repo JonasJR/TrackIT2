4c6630d4f50ddfc4f6fd805ae49c1a3a97ee375a
Jonas <jonas.remgard@gmail.com>
2016-05-17 15:46:34 +0200

    :tada: adds functionality for motivation form

--- app/controllers/motivations_controller.rb
+++ app/controllers/motivations_controller.rb
@@ -1,7 +1,24 @@
 class MotivationsController < ActionController::Base
 
   def create
+    if signed_in?
+      unless current_user.try(:company?)
+        motivation = Motivation.new(motivation_params)
+        motivation.user_id = current_user.id
+        motivation.proposal_id = params[:proposal_id]
 
+        if motivation.save
+          redirect_to proposals_url
+          flash[:success] = "Applied successfully"
+        else
+          render :show
+          flash[:error] = "An error occured, please try again!"
+        end
+      end
+    else
+      flash[:error] = "Please log in first"
+      redirect_to new_user_session_path
+    end
   end
 
   def show
